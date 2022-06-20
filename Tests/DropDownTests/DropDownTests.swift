d7e5895f0ecae13aa9c82168f6fbaeac3adf7943
Alessio <alessioiodiceuni@gmail.com>
2022-06-20 15:51:21 +0200

    Fixed the package

--- Tests/DropDownTests/DropDownTests.swift
+++ Tests/DropDownTests/DropDownTests.swift
@@ -1,14 +1,16 @@
-//import XCTest
-//@testable import DropDown
-//import SwiftUI
-//
-//final class DropDownTests: XCTestCase {
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct
-//        // results.
-//        XCTAssertEqual(DropDown(content: {
-//            Text("Hello world")
-//        }, size: .small, curtainColor: .black, chevronColor: .white))
-//    }
-//}
+import SwiftUI
+import XCTest
+@testable import DropDown
+
+@available(iOS 14.0, *)
+final class DropDownTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertNil((DropDown(content: {
+            Text("Hello world")
+        }, size: .small, curtainColor: .black, chevronColor: .white))
+        )
+    }
+}
